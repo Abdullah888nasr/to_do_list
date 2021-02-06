eb4a1dcf20b6b4026bc269625e496bee5c2b78f5
Abdullah888nasr <abdullah888nasr@gmail.com>
2021-02-07 00:01:04 +0200

    second file updated

--- includes/tpls/header.php
+++ includes/tpls/header.php
@@ -26,10 +26,10 @@
             
             <!--in login page it shouldn't appear username and drop menu part-->
             <?php if(pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME)!=='login'):?>
-            <div class="collapse navbar-collapse" id="app-nav">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username']; ?><span class="caret"></span></a>
+            <div class="navbar-collapse" id="app-nav">
+                <ul class="nav navbar-right">
+                    <li class="">
+                        <a href="#" class="" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="false"><?php echo $_SESSION['username']; ?><span class="caret"></span></a>
                         <ul class="dropdown-menu">
                         <li><a href="index.php?action=reset" class="confirm">Delete All Tasks</a></li>
                         <li><a href="login.php?action=logout">Logout</a></li>
